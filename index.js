9175d589c5f31ca7c4ec1ccd1767664857ed36b4
Andy Eccleston <eandy5000@yahoo.com>
2017-03-19 11:57:43 -0500

    basic server working

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 
+app.set('port', 3000)
+const port = app.get('port');
+
 app.get('/', (req, res) => {
     res.send("Hi there");
 });
 
-http.listen(3000, () => {
-    console.log(`listening on port: 3000`);
+http.listen(app.get('port'), () => {
+    console.log(`listening on port: ${port}`);
 });
