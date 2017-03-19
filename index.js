20dde808aab249df4a6cf4125cee07ccfe536708
Andy Eccleston <eandy5000@yahoo.com>
2017-03-19 12:16:15 -0500

    added styles and ul with form

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 'use strict'
 
 const express = require('express');
+const path = require('path');
 const app = express();
 const http = require('http').Server(app);
 
 app.set('port', 3000)
 const port = app.get('port');
 
-app.get('/', (req, res) => {
-    res.send("Hi there");
-});
+app.use(express.static(path.join(__dirname, 'client')));
 
 http.listen(app.get('port'), () => {
     console.log(`listening on port: ${port}`);
