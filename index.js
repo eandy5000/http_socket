b4ab6c680985c054db36e31a9b4b306ad440fc11
Andy Eccleston <eandy5000@yahoo.com>
2017-03-19 13:05:12 -0500

    basic socket connection

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.set('port', 3000)
 const port = app.get('port');
 
 app.use(express.static(path.join(__dirname, 'client')));
+
+io.on('connection', function(socket) {
+    console.log("user connected");
+    
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
+});
+
 
 http.listen(app.get('port'), () => {
     console.log(`listening on port: ${port}`);
