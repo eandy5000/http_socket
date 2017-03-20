0ffaa9a4dbe65680664b9c9afd571e8e7417788a
Andy Eccleston <eandy5000@yahoo.com>
2017-03-19 21:12:32 -0500

    chat example working

--- client/script.js
+++ client/script.js
@@ -1,3 +1,27 @@
-console.log('working');
 
-var socket = io();
+
+const socket = io();
+
+const form = document.querySelector('form');
+const message = document.querySelector('#m');
+const list = document.querySelector('#messages');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    socket.emit('chat message', message.value);
+    message.value = '';
+});
+
+socket.on('chat message', (msg) => {
+    const el = document.createElement('li');
+    
+    el.innerText = msg;
+
+    list.appendChild(el);
+    
+});
+
+
+
+
+
